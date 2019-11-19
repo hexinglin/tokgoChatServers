555e351ce02779f599bf523c3676af02b8106260
xinglin.He <1131515629@qq.com>
2019-11-19 22:19:13 +0800

    添加服务器转发

--- src/main/java/HomeCamera/TransferUDP.java
+++ src/main/java/HomeCamera/TransferUDP.java
@@ -1,35 +1,41 @@
 package HomeCamera;
 
+import Net.TCP.ServersBaseClass;
 import Net.UDP.UdpReceiveClass;
 
 import java.net.DatagramPacket;
+import java.net.Socket;
 import java.net.SocketException;
 
-public class TransferUDP extends UdpReceiveClass {
+public class TransferUDP extends ServersBaseClass {
 
-    private DatagramPacket sendPacket=null;
+    private Socket sendPacket=null;
     private int count = 0;
 
 
-    public TransferUDP() throws SocketException {
-        this.CreatReceiveMonitor(6000);
+    public TransferUDP() throws Exception {
+        this.CreatServer(6000,this);
 
-    }
-
-    public void setSendPacket(DatagramPacket sendPacket) {
-        this.sendPacket = sendPacket;
-        this.count = 100;
     }
 
     @Override
-    protected void Receive(DatagramPacket datagramPacket, byte[] bytes, int i) {
+    protected void ClientExit(Socket socket) {
+
+    }
+
+    @Override
+    public void Receive(Socket socket, byte[] bytes, int i) {
         System.out.println(i);
-        if (this.sendPacket==null){
+        if (this.sendPacket!=null){
             if (this.count>0){
                 this.Send(this.sendPacket,bytes,i);
                 this.count--;
                 System.out.println("转发");
             }
+        }
+        if (i == 6){
+            sendPacket = socket;
+            this.count =100;
         }
     }
 }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,16 +1,14 @@
+import Net.TCP.ClientBaseClass;
 import Net.UDP.UdpReceiveClass;
 import Net.UDP.UdpSendClass;
 import com.alibaba.fastjson.JSON;
 
 import java.io.UnsupportedEncodingException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class test extends UdpReceiveClass {
+public class test extends ClientBaseClass {
 
 
     public static void main(String[] args){
@@ -25,30 +23,38 @@ public class test extends UdpReceiveClass {
     }
 
     public test() throws Exception{
-        this.CreatReceiveMonitor(6000);
+        this.Connect("hecao.pw",20001,this);
         byte[] dd = "action".getBytes("gbk");
-        this.Send(dd,dd.length,8000, InetAddress.getByName("118.199.210.122"));
+        this.Send(dd,dd.length);
+//        this.Send(dd,dd.length,8000, InetAddress.getByName("118.199.210.122"));
         Map<String,Object> k = new HashMap<>();
         k.put("mod","homeCamera");
         k.put("action","feedback");
         k.put("identity","viviter");
         k.put("NO",0);
         byte[] se= JSON.toJSONString(k).getBytes("gbk");
-        this.Send(se,se.length,20001, InetAddress.getByName("hecao.pw"));
+        UdpSendClass udpSendClass = new UdpSendClass(6000);
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        this.Send(se,se.length,8000, InetAddress.getByName("118.199.210.122"));
-//        this.Send(se.getBytes("gbk"),se.length(),8000, InetAddress.getByName("192.168.1.102"));
-        System.out.println("fdgasdfadsds");
+        udpSendClass.SendData(se,0,se.length,20001, InetAddress.getByName("hecao.pw"));
+
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        this.Send(se,se.length,8000, InetAddress.getByName("118.199.210.122"));
+////        this.Send(se.getBytes("gbk"),se.length(),8000, InetAddress.getByName("192.168.1.102"));
+//        System.out.println("fdgasdfadsds");
+    }
+
+    @Override
+    protected void ServersBreakoff() {
+
     }
 
 
     @Override
-    protected void Receive(DatagramPacket datagramPacket, byte[] bytes, int i) {
+    public void Receive(Socket socket, byte[] bytes, int i) {
         System.out.println(i);
     }
 }
